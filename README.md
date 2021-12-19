a7882a4e5b23bd39e049ea9d1d59f00257f823f9
Gustav OL <70300539+Gustav-Dev17@users.noreply.github.com>
2021-12-19 13:21:21 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 1money api
+ 
---
+++ index.js
@@ -0,0 +1,12 @@
+const { User } = require('./app/models');
+const express = require('express');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => console.log("Server is running"));

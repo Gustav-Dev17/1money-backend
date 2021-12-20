6a3e5597190f678afc68e6fdfaa4cce1d4d65851
Gustav OL <70300539+Gustav-Dev17@users.noreply.github.com>
2021-12-20 11:53:21 -0400

    add feature hash password before update

--- app/controllers/UpdateUserController.js
+++ app/controllers/UpdateUserController.js
@@ -1,8 +1,9 @@
 const { User } = require("../models");
+const bcrypt = require("bcrypt");
 
 const UpdateUserController = async (req, res, next) => {
   const { id } = req.params;
-  const { name, email, password } = req.body;
+  const { name, email, password} = req.body;
   const user = await User.findByPk(id);
   if (!user) {
     return res.status(404).json({ message: "User not found" });
@@ -10,6 +11,7 @@ const UpdateUserController = async (req, res, next) => {
   user.name = name ? name : user.name;
   user.email = email ? email : user.email;
   user.password = password ? password : user.password;
+
   const result = await user.save();
   return res.json(result);
 };
--- app/models/user.js
+++ app/models/user.js
@@ -5,18 +5,32 @@ module.exports = (sequelize, DataTypes) => {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
+  },
+  {
+    hooks: {
+     beforeCreate: async (user) => {
+      if (user.password) {
+       const salt = await bcrypt.genSaltSync(10, 'a');
+       user.password = bcrypt.hashSync(user.password, salt);
+      }
+     },
+     beforeUpdate:async (user) => {
+      if (user.password) {
+       const salt = await bcrypt.genSaltSync(10, 'a');
+       user.password = bcrypt.hashSync(user.password, salt);
+      }
+     }
+    },
+    // instanceMethods: {
+    //  validPassword: (password) => {
+    //   return bcrypt.compareSync(password, this.password);
+    //  }
+    // }
   });
 
-  User.beforeCreate((user, options) => {
-
-    return bcrypt.hash(user.password, 10)
-        .then(hash => {
-            user.password = hash;
-        })
-        .catch(err => { 
-            throw new Error(); 
-        });
-});
+  //  User.prototype.validPassword = async (password, hash) => {
+  //   return await bcrypt.compareSync(password, hash);
+  // }
 
   return User;
 };

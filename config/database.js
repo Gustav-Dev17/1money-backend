d858dbfc5d6a65b0d19243baf361150b5a400a08
Gustav OL <70300539+Gustav-Dev17@users.noreply.github.com>
2021-12-20 00:22:39 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,18 +1,11 @@
 require("dotenv").config();
 
 module.exports = {
-
-  username: "root",
-  password: "",
-  database: "money",
-  host: "127.0.0.1",
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  host: process.env.HOST,
   dialect: "mysql",
-
-  // username: process.env.USERNAME,
-  // password: process.env.PASSWORD,
-  // database: process.env.DATABASE,
-  // host: process.env.HOST,
-  // dialect: "mysql",
 };
 
 

4aea7ab0ad8a0b9fca9efe0bc0c56eb345dc9ec5
Gustav OL <70300539+Gustav-Dev17@users.noreply.github.com>
2021-12-24 15:35:18 -0400

    feat: add courses migration

---
+++ database/migrations/20211224192357-create-courses.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, DataTypes) => {
+    return queryInterface.createTable("Courses", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      description: {
+        allowNull: true,
+        type: DataTypes.STRING,
+      },
+      duration: {
+        allowNull: true,
+        type: DataTypes.FLOAT,
+      },
+      price: {
+        allowNull: true,
+        type: DataTypes.FLOAT,
+      },
+      cover: {
+        allowNull: true,
+        type: DataTypes.STRING,
+      },
+      prevideo: {
+        allowNull: true,
+        type: DataTypes.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    });
+  },
+  
+  down: (queryInterface) => {
+    return queryInterface.dropTable("Courses");
+  },
+};

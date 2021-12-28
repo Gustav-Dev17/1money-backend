f98bb6fa617d2a559533ddc2cdcbe4b6f79ecac6
Douglas Filipe <douglasfelipe.net77@gmail.com>
2021-12-28 14:55:33 -0400

    feat: add update user

--- app/controllers/user/UpdateUserController.ts
+++ app/controllers/user/UpdateUserController.ts
@@ -1,30 +1,37 @@
-const bcrypt = require("bcrypt");
-const { User } = require("../../models");
+import { getRepository } from "typeorm";
+import { Users } from "../../entities/User";
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
 
-const UpdateUserController = async (req, res, next) => {
+const UpdateUserController = async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
+    const repo = getRepository(Users);
     const { name, email, password, picture } = req.body;
-    const userEmail = await User.findOne({ where: { email: req.body.email } });
-    const hashPassword = await bcrypt.hash(password, 10);
-    const user = await User.findByPk(id);
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
+    const userEmail = await repo.findOne({email: email});
     if (userEmail) {
       return res.status(409).json({ message: "Email already exists" });
     }
-    
+    const hashPassword = await bcrypt.hash(password, 10);
+    const user = await repo.findOne(id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
     user.name = name ? name : user.name;
     user.email = email ? email : user.email;
     user.password = password ? hashPassword : user.password;
     user.picture = picture ? picture : user.picture;
 
-    await user.save();
-    return res.json({"name": user.name, "email": user.email, "picture": user.picture });
+    await repo.save(user);
+    return res.json({
+      name: user.name,
+      email: user.email,
+      picture: user.picture,
+    });
   } catch {
     return res.status(500).json({ message: "Error" });
   }
 };
 
-module.exports = { UpdateUserController };
+export default UpdateUserController;
--- app/routes/userRoute.ts
+++ app/routes/userRoute.ts
@@ -1,17 +1,16 @@
-//const verifyToken = require("../middlewares/VerifyToken");
-
 import { CreateUserController } from "../controllers/user/CreateUserController";
 import { Router } from "express";
+const router = Router();
+
 import LoginUserController from "../controllers/user/LoginUserController";
 import DeleteUserController from "../controllers/user/DeleteUserController";
 import ShowUserController from "../controllers/user/ShowUserController";
 import verifyToken from "../middlewares/VerifyToken";
-
-const router = Router();
+import UpdateUserController from "../controllers/user/UpdateUserController";
 
 router.post("/register", CreateUserController);
 router.post("/login", LoginUserController);
-//router.put("/:id", verifyToken, UpdateUserController);
+router.put("/:id", verifyToken, UpdateUserController);
 router.get("/:id", verifyToken, ShowUserController);
 router.delete("/:id", verifyToken, DeleteUserController);
 

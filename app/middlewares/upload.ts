571f34286ecdf136ceb79c7a9f71ea771cb6230c
Douglas Filipe <douglasfelipe.net77@gmail.com>
2021-12-31 16:49:46 -0400

    fix: verify token type id

---
+++ app/middlewares/upload.ts
@@ -0,0 +1,13 @@
+const multer = require("multer");
+module.exports = multer({
+  storage: multer.memoryStorage(),
+  fileFilter: (req, file, cb) => {
+    const isAccepted = ["video/mp4"].find(
+      (formatoAceito) => formatoAceito == file.mimetype
+    );
+    if (isAccepted) {
+      return cb(null, true);
+    }
+    return cb(null, false);
+  },
+});

0ed27fab8084c7e4248f0eb150b743680d333014
Gustav OL <70300539+Gustav-Dev17@users.noreply.github.com>
2022-01-01 16:21:29 -0400

    feat: add getadmin controller and route

---
+++ app/controllers/admin/GetAdminController.ts
@@ -0,0 +1,20 @@
+import { getRepository } from "typeorm";
+import { Users } from "../../entities/User";
+import { Request, Response } from "express";
+
+const GetAdminController = async (req: Request, res: Response) => {
+  try {
+    const { id } = req;
+    const repo = getRepository(Users);
+    const user = await repo.findOne(id);
+    return res.json({
+      name: user.name,
+      email: user.email,
+      picture: user.picture,
+    });
+  } catch {
+    return res.status(500).json({ message: "Error " });
+  }
+};
+
+export default GetAdminController;
--- app/routes/adminRoute.ts
+++ app/routes/adminRoute.ts
@@ -9,10 +9,12 @@ import { GetAllLessonsInACoursesController } from "../controllers/admin/course/G
 import { CreateAdminController } from "../controllers/admin/CreateAdminController";
 import { LoginAdminController } from "../controllers/admin/LoginAdminController";
 import { verifyTokenAdmin } from "../middlewares/VerifyTokenAdmin";
+import GetAdminController from "../controllers/admin/GetAdminController";
 import multerConfig from "../config/multer";
 
 router.post("/register", CreateAdminController);
 router.post("/login", LoginAdminController);
+router.get("/", GetAdminController);
 router.get("/course", GetAllCoursesController);
 router.post("/course", verifyTokenAdmin, CreateCourseController);
 router.post(

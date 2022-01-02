68ee73682ef6730682d33cacae871a064b923a71
Gustav OL <70300539+Gustav-Dev17@users.noreply.github.com>
2022-01-02 14:57:00 -0400

    fix: delete lesson controller

--- app/controllers/admin/lesson/DeleteLessonController.ts
+++ app/controllers/admin/lesson/DeleteLessonController.ts
@@ -7,22 +7,14 @@ const s3 = new aws.S3();
 export const DeleteLessonController = async (req: Request, res: Response) => {
     try {
       const repo = getRepository(Lessons);
-      const { name, sequence, duration, resourse } = req.body;
       const lesson = await repo.findOne({ id: req.params.id });
-      if (!req.file) {
+      if (!lesson) {
         return res.status(404).json({ message: "Lesson not found" });
-      }
-      const { originalname: size, key, location: url } = req.file;
-        lesson.name = name ? name : lesson.name;
-        lesson.sequence = sequence ? sequence : lesson.sequence;
-        lesson.duration = duration ? duration : lesson.duration;
-        lesson.resource = resourse ? resourse : lesson.resource;
-        s3.deleteObject({
-            Bucket: "uploadfileteste2",
-            Key: lesson.key,
-        }).promise();
-        lesson.video = url;
-        lesson.key = key;
+      }    
+      s3.deleteObject({
+        Bucket: "uploadfileteste2",
+        Key: lesson.key,
+      }).promise();
       await repo.delete(lesson);
       return res.status(200).json({ message: "Lesson deleted from the course!" });
     } catch {

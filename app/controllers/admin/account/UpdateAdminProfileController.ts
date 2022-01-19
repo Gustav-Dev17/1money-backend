76e68cae651948c6dfda63f9e29c77e70227d26b
Gustav OL <70300539+Gustav-Dev17@users.noreply.github.com>
2022-01-19 11:13:09 -0400

    feat: add update admin profile

---
+++ app/controllers/admin/account/UpdateAdminProfileController.ts
@@ -0,0 +1,35 @@
+import { getRepository } from "typeorm";
+import { Users } from "../../../entities/User";
+import { Request, Response } from "express";
+import {
+  deletePhoto,
+  uploadPhotoProfile,
+} from "../../../../utils/uploadPhotoProfile";
+
+export const UpdateAdminProfileController = async (req: Request, res: Response) => {
+  try {
+    const { id } = req;
+    const repo = getRepository(Users);
+    const photo = req.file;
+    const user = await repo.findOne(id);
+    if (!user) {
+      return res.status(404).json({ message: "Admin not found" });
+    }
+    if (!photo) {
+      return res.status(404).json({ message: "Photo not found" });
+    }
+    const { buffer }: any = photo;
+    if (photo) {
+      const key = user.key ? user.key : "key-aleatorio";
+      await deletePhoto(key);
+    }
+    const file = await uploadPhotoProfile(buffer, photo);
+    user.picture = photo ? file.Location : user.picture;
+    user.key = photo ? file.Key : user.key;
+
+    await repo.save(user);
+    return res.json({ profile: user.picture });
+  } catch {
+    return res.status(500).json({ message: "Error" });
+  }
+};

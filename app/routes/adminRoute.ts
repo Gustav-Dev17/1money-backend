a72248ab5559613222055fd8010a628373ea52d1
Gustav OL <70300539+Gustav-Dev17@users.noreply.github.com>
2022-01-19 11:13:36 -0400

    fix: sorting imports in routes

--- app/routes/adminRoute.ts
+++ app/routes/adminRoute.ts
@@ -1,17 +1,20 @@
-import { Router } from "express";
 import multer from "multer";
-import { CreateCourseController } from "../controllers/admin/course/CreateCourseController";
-import { GetAllCoursesController } from "../controllers/admin/course/GetAllCoursesController";
-import { CreateLessonController } from "../controllers/admin/lesson/CreateLessonController";
-import { GetAllLessonsInACourseController } from "../controllers/admin/course/GetAllLessonsInACourseController";
-import { CreateAdminController } from "../controllers/admin/account/CreateAdminController";
-import { LoginAdminController } from "../controllers/admin/account/LoginAdminController";
-import { verifyTokenAdmin } from "../middlewares/VerifyTokenAdmin";
-import GetAdminController from "../controllers/admin/account/GetAdminController";
+import multerPhoto from "../config/multerPhoto";
 import multerVideo from "../config/multerVideo";
+import GetAdminController from "../controllers/admin/account/GetAdminController";
+import UpdateAdminController from "../controllers/admin/account/UpdateAdminController";
+
+import { Router } from "express";
+import { verifyTokenAdmin } from "../middlewares/VerifyTokenAdmin";
+import { LoginAdminController } from "../controllers/admin/account/LoginAdminController";
+import { CreateAdminController } from "../controllers/admin/account/CreateAdminController";
+import { CreateCourseController } from "../controllers/admin/course/CreateCourseController";
+import { CreateLessonController } from "../controllers/admin/lesson/CreateLessonController";
 import { UpdateLessonController } from "../controllers/admin/lesson/UpdateLessonController";
 import { DeleteLessonController } from "../controllers/admin/lesson/DeleteLessonController";
-import UpdateAdminController from "../controllers/admin/account/UpdateAdminController";
+import { GetAllCoursesController } from "../controllers/admin/course/GetAllCoursesController";
+import { UpdateAdminProfileController } from "../controllers/admin/account/UpdateAdminProfileController";
+import { GetAllLessonsInACourseController } from "../controllers/admin/course/GetAllLessonsInACourseController";
 
 const router = Router();
 
@@ -20,6 +23,12 @@ router.post("/register", CreateAdminController);
 router.post("/login", LoginAdminController);
 router.get("/", verifyTokenAdmin, GetAdminController);
 router.put("/", verifyTokenAdmin, UpdateAdminController);
+router.put(
+  "/profile",
+  verifyTokenAdmin,
+  multer(multerPhoto).single("photo"),
+  UpdateAdminProfileController
+);
 
 //courses routes
 router.post("/course", verifyTokenAdmin, CreateCourseController);
@@ -41,6 +50,5 @@ router.put(
   UpdateLessonController
 );
 router.delete("/lesson/:id", verifyTokenAdmin, DeleteLessonController);
-
 
 export default router;
--- app/routes/userRoute.ts
+++ app/routes/userRoute.ts
@@ -1,24 +1,24 @@
-import { CreateUserController } from "../controllers/user/account/CreateUserController";
-import { Router } from "express";
-import LoginUserController from "../controllers/user/account/LoginUserController";
-import DeleteUserController from "../controllers/user/account/DeleteUserController";
-import GetUserController from "../controllers/user/account/GetUserController";
-import verifyToken from "../middlewares/VerifyToken";
-import UpdateUserController from "../controllers/user/account/UpdateUserController";
-import { UpdateUserProfileController } from "../controllers/user/account/UpdateUserProfileController";
 import multer from "multer";
 import multerPhoto from "../config/multerPhoto";
-import { GetAllCoursesController } from "../controllers/user/course/GetAllCoursesController";
-import { GetAllLessonsInACourseController } from "../controllers/admin/course/GetAllLessonsInACourseController";
-import { AddCourseToCartController } from "../controllers/user/cart/AddCourseToCartController";
-import { ListCoursesFromCartController } from "../controllers/user/cart/ListCoursesFromCartController";
+import verifyToken from "../middlewares/VerifyToken";
+import GetUserController from "../controllers/user/account/GetUserController";
+import LoginUserController from "../controllers/user/account/LoginUserController";
+import UpdateUserController from "../controllers/user/account/UpdateUserController";
+import DeleteUserController from "../controllers/user/account/DeleteUserController";
+
+import { Router } from "express";
 import { BuyCourseController } from "../controllers/user/purchase/BuyCourseController";
+import { CreateUserController } from "../controllers/user/account/CreateUserController";
+import { GetAllCoursesController } from "../controllers/user/course/GetAllCoursesController";
+import { AddCourseToCartController } from "../controllers/user/cart/AddCourseToCartController";
+import { UpdateUserProfileController } from "../controllers/user/account/UpdateUserProfileController";
+import { ListCoursesFromCartController } from "../controllers/user/cart/ListCoursesFromCartController";
 import { RemoveCourseFromCartController } from "../controllers/user/cart/RemoveCourseFromCartController";
+import { GetAllLessonsInACourseController } from "../controllers/admin/course/GetAllLessonsInACourseController";
 
 const router = Router();
 
 //profile routes
-
 router.post("/register", CreateUserController);
 router.post("/login", LoginUserController);
 router.put("/", verifyToken, UpdateUserController);
@@ -32,18 +32,15 @@ router.put(
 );
 
 //courses routes
-
 router.get("/courses", GetAllCoursesController);
 router.get("/course/:id", GetAllLessonsInACourseController);
 
 //cart routes
-
 router.post("/cart", verifyToken, AddCourseToCartController);
 router.get("/cart", verifyToken, ListCoursesFromCartController);
 router.delete("/cart/:id", verifyToken, RemoveCourseFromCartController)
 
 //purchase routes
-
 router.post("/purchase", verifyToken, BuyCourseController);
 
 export default router;

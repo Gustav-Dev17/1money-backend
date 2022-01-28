682670e71bfb622f03d818d8ba76b0c65d04d002
Gustav OL <70300539+Gustav-Dev17@users.noreply.github.com>
2022-01-28 11:50:41 -0400

    fix: index.ts

--- index.ts
+++ index.ts
@@ -8,26 +8,13 @@ import cors from "cors";
 
 const app = express();
 
-const options = {
-  origin: 'http://localhost:3000',
-  // methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  // preflightContinue: false,
-  // optionsSuccessStatus: 204,
-  credentials: true
-};
+app.use(cors());
+
+app.options('*', cors())
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
-
-app.use(cors(options));
-
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  next();
-});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");

364b47f3237aca9ac0d8f91be5257245fca26523
Gustav OL <70300539+Gustav-Dev17@users.noreply.github.com>
2022-01-28 11:23:16 -0400

    fix: credentials

--- index.ts
+++ index.ts
@@ -22,6 +22,13 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });

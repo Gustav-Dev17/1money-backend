9cb059d620fc61750cf0407b51f849e0aafd7f23
Douglas Filipe <douglasfelipe.net77@gmail.com>
2022-01-28 10:45:12 -0400

    fix: add credentials

--- custom_typings/express/index.d.ts
+++ custom_typings/express/index.d.ts
@@ -23,3 +23,4 @@ declare namespace Express {
     };
   }
 }
+
--- dist/index.js
+++ dist/index.js
@@ -12,7 +12,7 @@ const adminRoute_1 = __importDefault(require("./app/routes/adminRoute"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)({ origin: "*" }));
+app.use((0, cors_1.default)({ origin: "*", credentials: true }));
 app.use(body_parser_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
